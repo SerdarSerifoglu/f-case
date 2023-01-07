cea300e37a0888bf6813e92253a7d4b8a37967f1
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2023-01-07 10:02:29 +0300

    ArtistList infinite scroll completed

--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -1,29 +1,51 @@
-import React from "react";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
+import React, { useEffect, useState } from "react";
+import { useQuery, useInfiniteQuery } from "react-query";
+
 import Card from "./Card";
 import styles from "./artistList.module.css";
 
+const ITEMS_PER_PAGE = 30;
+
 const ArtistList = () => {
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+
   const apiKey = "007d8369d031d1645e0eba2eb1f053fb";
-  const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${apiKey}&format=json`;
+  const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${apiKey}&format=json&page=${page}&limit=${ITEMS_PER_PAGE}`;
 
-  const { data, status } = useQuery("artistListTest", () =>
-    fetch(apiUrl).then((res) => res.json())
-  );
+  function fetchMoreItems() {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((newItems) => {
+        if (newItems.artists.artist.length === 0) {
+          return;
+        }
+        setItems([...items, ...newItems.artists.artist]);
+      });
+  }
 
-  if (status === "loading") return <p>Loading...</p>;
-  if (status === "error") return <p>Error: </p>;
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    fetchMoreItems();
+  }, [page]);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >=
+      document.documentElement.scrollHeight
+    ) {
+      setPage((prev) => prev + 1);
+    }
+  };
 
   return (
     <React.Fragment>
-      <div className={styles["artist-list"]}>
-        {data.artists.artist.map((e, i) => (
+      <div onScroll={handleScroll} className={styles["artist-list"]}>
+        {items.map((e, i) => (
           <Card
             key={i}
             data={{ ...e, imageUrl: e.image[2]["#text"], type: "artist" }}
