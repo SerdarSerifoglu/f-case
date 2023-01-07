dbeb03f577659d2b612dba9ed1dd94a17b54d40c
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2023-01-07 10:37:21 +0300

    Card component refactor

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,38 +5,85 @@ import React from "react";
 const Card = ({ data }) => {
   const { mbid, type, name, imageUrl, listeners, playcount, author } = data;
 
-  return (
-    <React.Fragment key={mbid}>
-      <div className={styles.card}>
-        <div className={styles.card__image}>
-          <img src={imageUrl} alt={`${name} image`} />
-        </div>
-        <div className={styles.card__info}>
-          {type === cardTypes.artist ? (
+  if (type === cardTypes.artist) {
+    return (
+      <React.Fragment key={mbid}>
+        <div className={styles.card}>
+          <div className={styles.card__image}>
+            <img src={imageUrl} alt={`${name} image`} />
+          </div>
+          <div className={styles.card__info}>
             <div className={styles.card__title}>Artist</div>
-          ) : null}
-          <div className={styles.card__name}>{name ?? null}</div>
-          {type === cardTypes.album || type === cardTypes.track ? (
-            <div className={styles.card__author}>{author ?? null}</div>
-          ) : null}
-        </div>
-        <div className={styles.card__statistics}>
-          {type === cardTypes.artist || type === cardTypes.track ? (
+            <div className={styles.card__name}>{name ?? null}</div>
+          </div>
+          <div className={styles.card__statistics}>
             <div className={styles.card__listenerscount}>
-              {type === cardTypes.artist
-                ? `listeners: ${listeners}`
-                : `${listeners} listeners`}
+              listeners: {listeners}
             </div>
-          ) : null}
-          <div className={styles.card__playcount}>
-            {type === cardTypes.artist
-              ? `playcount: ${playcount}`
-              : `${playcount} playcount`}
+            <div className={styles.card__playcount}>playcount: {playcount}</div>
           </div>
         </div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
+
+  if (type === cardTypes.album) {
+    return (
+      <React.Fragment key={mbid}>
+        <div className={styles.card}>
+          <div className={styles.card__image}>
+            <img src={imageUrl} alt={`${name} image`} />
+          </div>
+          <div className={styles.card__info}>
+            <div className={styles.card__name}>{name ?? null}</div>
+            <div className={styles.card__author}>{author ?? null}</div>
+          </div>
+          <div className={styles.card__statistics}>
+            <div className={styles.card__playcount}>{playcount} playcount</div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  if (type === cardTypes.track) {
+    return (
+      <React.Fragment key={mbid}>
+        <div className={styles.card}>
+          <div className={styles.card__image}>
+            <img src={imageUrl} alt={`${name} image`} />
+          </div>
+          <div className={styles.card__info}>
+            <div className={styles.card__name}>{name ?? null}</div>
+            <div className={styles.card__author}>{author ?? null}</div>
+          </div>
+          <div className={styles.card__statistics}>
+            <div className={styles.card__listenerscount}>
+              {listeners} listeners
+            </div>
+
+            <div className={styles.card__playcount}>{playcount} playcount</div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  if (type === cardTypes.title) {
+    return (
+      <React.Fragment key={mbid}>
+        <div className={styles.card}>
+          <div className={styles.card__image}>
+            <img src={imageUrl} alt={`${name} image`} />
+          </div>
+          <div className={styles.card__info}>
+            <div className={styles.card__name}>{name ?? null}</div>
+          </div>
+          <div className={styles.card__statistics}></div>
+        </div>
+      </React.Fragment>
+    );
+  }
 };
 
 export default Card;
